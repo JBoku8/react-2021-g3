5357e6fe3eda651ad2471e6ca15a40099c66f133
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-06-19 15:35:27 +0400

    react-testing-perf-optimization

---
+++ src/components/greeting/greeting.test.js
@@ -0,0 +1,5 @@
+import { render } from '@testing-library/react';
+
+describe('Greeting component test', () => {
+  it('should render default title', () => {});
+});
---
+++ src/utils/testids.js
@@ -0,0 +1,5 @@
+export const TEST_IDS = {
+  greeting: {
+    title: 'greeting.title',
+  },
+};

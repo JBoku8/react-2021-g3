07924d4b9ee5d98cb346feb04aa4891250d4f22b
Jano Bokuchava <jano.bokuchava@btu.edu.ge>
2021-06-26 15:43:04 +0400

    React testing perf optimization (#15)
    * react-testing-perf-optimization
    
    * react-testing-perf-optimization
    
    * react-testing-perf-optimization
    
    * updates
    
    * updates
    
    * react-testing-perf-optimization
    
    Co-authored-by: Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { validationMiddleware, autoLoginMiddleware } from './middleware';
 import rootReducer from './reducers';
 
-const middleware = [validationMiddleware, autoLoginMiddleware, thunk, logger];
+const middleware = [validationMiddleware, autoLoginMiddleware, thunk];
 
 const rootStore = createStore(rootReducer, applyMiddleware(...middleware));
 
